1f1ac9829d55c0f1db65d55bfd24c0387edd9713
Shuv2202 <shuvm2000@gmail.com>
2026-08-22 00:13:30 +0530

    Set KDS (Kitchen Display System) as exclusive root component for this repository

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import Kitchen from './Kitchen.tsx'
-
-// Simple client-side routing check
-const isKitchen = window.location.pathname === '/kitchen' || new URLSearchParams(window.location.search).get('view') === 'kitchen';
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {isKitchen ? <Kitchen /> : <App />}
+    <Kitchen />
   </StrictMode>,
 )
